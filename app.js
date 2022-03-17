5de7462038a56620b01d82464d87c9009934e360
lake <ogu5959@gmail.com>
2022-03-17 22:13:09 +0900

    config / sleep 기능 추가

--- app.js
+++ app.js
@@ -1,35 +1,28 @@
+const config = require('./config')
+
 // 25개 전화번호를 가진 리스트 필요
 // 스크립트를 실행하면 while 문안에서 무한으로 처리
 // For문을 돌려서 처리 한건마다 요청
 // 1건만다 1~2초 슬립
 // 모든건이 끝나면 20초 슬립
 
-
-const list = [
-    {indx : 0, phone: "1"},
-    {indx : 1, phone: "2"},
-    {indx : 2, phone: "3"},
-    {indx : 3, phone: "4"},
-    {indx : 4, phone: "5"},
-    {indx : 5, phone: "6"},
-    {indx : 6, phone: "7"},
-    {indx : 7, phone: "8"},
-    {indx : 8, phone: "9"},
-    {indx : 9, phone: "10"},
-]
+// 슬립 설정
+const lastSleep = 5000
+const midSleep  = 1000
 
 // 메인 실행 함수
 const main = () => {
+
+    const arrCnt = config.list.length-1
+    let ms
+
     while(true) {
-        for(i in list) {
-            console.log(list[i].phone + " [" + (parseInt(i)+1) + "] " + list.length)
-            if ( i == (list.length-1) ) {
-                console.log("마지막")
-                sleep(5000)
-            } else {
-                sleep(1000)
-            }
-            
+        for(i in config.list) {
+            console.log(config.list[i].phone + " [" + i + "] " + arrCnt)
+
+            // 마지막일경우 5초 중간은 1초
+            ms = ( i == arrCnt ) ? lastSleep : midSleep;
+            sleep(ms)
         }
     }
 }
---
+++ config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    dev: "TEST",
+    list: [
+        {indx : 0, phone: "1"},
+        {indx : 1, phone: "2"},
+        {indx : 2, phone: "3"},
+        {indx : 3, phone: "4"},
+        {indx : 4, phone: "5"},
+        {indx : 5, phone: "6"},
+        {indx : 6, phone: "7"},
+        {indx : 7, phone: "8"},
+        {indx : 8, phone: "9"},
+        {indx : 9, phone: "10"},
+    ] 
+}
